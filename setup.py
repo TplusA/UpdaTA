666c9af2a334368da440248b64fce737ad96b9ec
Robert Tiemann <r.tiemann@ta-hifi.com>
2020-06-02 13:41:36 +0200

    setup: Add basic setuptools script.
    Change-Id: I95cd6f339c8180c50b71e035047aa10b1a119948

---
+++ setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  T+A elektroakustik GmbH & Co. KG
+#
+# This file is part of UpdaTA
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='UpdaTA',
+    version='0.1',
+    maintainer='Robert Tiemann',
+    maintainer_email='R.Tiemann@ta-hifi.de',
+    packages=find_packages(),
+    scripts=['updata_determine_strategy.py', 'updata_execute.py'],
+)
