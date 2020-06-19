20b52c2615e3686b8aa2a681efb06352532487f1
Robert Tiemann <r.tiemann@ta-hifi.com>
2020-06-19 11:15:37 +0200

    update: Add shell script template which wraps updata_execute.py.
    The purpose of this script is to support creating a standalone program
    which cannot be uninstalled by the package manager during an update.
    It can be run from the REST API, from systemd, or any other suitable
    to start or resume an update.
    
    To generate a working script, the following patterns must be
    substituted: @THE_PLAN@, @STAMP_DIR@, and @ALLOW_EXECUTION@.
    
    Pattern @THE_PLAN@ must be replaced by an absolute filename of an
    update plan as generated by updata_determine_strategy.py.
    
    Pattern @STAMP_DIR@ must be replaced by an absolute directory in which
    this script can maintain its state files.
    
    Pattern @ALLOW_EXECUTION@ must be replaced by the string "yes". This
    is a guard against running the template file directly.
    
    Change-Id: I4a4e431d4f496cc6e0492b3a3c1b222605c9f503

---
+++ updata_system_update.template.sh
@@ -0,0 +1,100 @@
+#! /bin/sh
+#
+# Perform update, keep state in file system.
+#
+# Exit codes:
+#    0  Done, no errors
+#    3  Tried to run this template script directly
+#    5  The update plan does not exist
+#    6  Status file directory does not exist
+#   10  Update step failed (first try)
+#   11  Update step failed (second try)
+#   12  Update not attempted again due to previous multiple failures (most
+#       probably a recovery is needed)
+#   20  Reboot step failed for some reason
+#   21  Reboot step failed because the reboot request failed
+#
+# State files (maintained in @STAMP_DIR@):
+#   update_started
+#   update_done
+#   update_failure
+#   update_failure_again
+#   update_reboot_started
+#   update_reboot_stderr
+#   update_reboot_failed
+#
+
+test "x@ALLOW_EXECUTION@" = "xyes" || exit 3
+
+THE_PLAN="@THE_PLAN@"
+STAMP_DIR="@STAMP_DIR@"
+
+test -f "${THE_PLAN}" || exit 5
+test -d "${STAMP_DIR}" || exit 6
+
+UPDATE_BEGIN="${STAMP_DIR}/update_started"
+UPDATE_DONE="${STAMP_DIR}/update_done"
+UPDATE_FAIL="${STAMP_DIR}/update_failure"
+UPDATE_FAIL_AGAIN="${STAMP_DIR}/update_failure_again"
+REBOOT_BEGIN="${STAMP_DIR}/update_reboot_started"
+REBOOT_STATUS="${STAMP_DIR}/update_reboot_stderr"
+REBOOT_FAIL="${STAMP_DIR}/update_reboot_failed"
+
+test -f "${UPDATE_DONE}" || rm -f "${UPDATE_BEGIN}"
+test -f "${REBOOT_FAIL}" || rm -f "${REBOOT_BEGIN}"
+
+test -f "${UPDATE_FAIL_AGAIN}" && exit 12
+
+if test -f "${UPDATE_BEGIN}" && test -f "${UPDATE_FAIL}"
+then
+    touch "${UPDATE_FAIL_AGAIN}"
+    rm -f "${UPDATE_BEGIN}"
+fi
+
+if test ! -f "${UPDATE_BEGIN}"
+then
+    rm -f "${UPDATE_DONE}" "${UPDATE_FAIL}" "${REBOOT_BEGIN}"
+    touch "${UPDATE_BEGIN}"
+    sync
+
+    updata_execute.py --avoid-reboot -p "${THE_PLAN}" 2>"${UPDATE_FAIL}"
+    RET=$?
+
+    # we are done with the update step
+    touch "${UPDATE_DONE}"
+
+    if test ${RET} -ne 0
+    then
+        logger <"${UPDATE_FAIL}"
+
+        test -f "${UPDATE_FAIL_AGAIN}" && exit 11
+        exit 10
+    fi
+
+    # no error: remove the failure markers
+    rm -f "${UPDATE_FAIL}" "${UPDATE_FAIL_AGAIN}"
+fi
+
+if test ! -f "${REBOOT_BEGIN}"
+then
+    rm -f "${REBOOT_STATUS}" "${REBOOT_FAIL}"
+    touch "${REBOOT_BEGIN}"
+    sync
+
+    updata_execute.py --reboot-only -p "${THE_PLAN}" 2>"${REBOOT_STATUS}"
+    RET=$?
+
+    # reboot was possibly triggered, therefore it is possible (though unlikely)
+    # that we do not reach this point in case the script returned with no
+    # error; in case of no error, we simply do nothing
+    test ${RET} -eq 0 && exit 0
+
+    # failed to reboot
+    touch "${REBOOT_FAIL}"
+    logger <"${REBOOT_STATUS}"
+
+    test ${RET} -eq 10 || exit 20
+    exit 21
+fi
+
+exit 0
