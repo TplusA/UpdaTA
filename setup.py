d650cffde9e7543737d0d9f8d0a63438d04455f4
Robert Tiemann <r.tiemann@ta-hifi.com>
2021-03-24 17:45:30 +0100

    Bump version to 0.6.
    Change-Id: I9be378528fb3d5c8e6a4f43a5371106b8d3067f2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='UpdaTA',
-    version='0.5',
+    version='0.6',
     maintainer='Robert Tiemann',
     maintainer_email='R.Tiemann@ta-hifi.de',
     packages=find_packages(),
